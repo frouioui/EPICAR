76ce7e1fab4530669ec93396deebb5f2825452ab
Tauran Julien <tjulien@localhost.localdomain>
2018-02-24 19:23:33 +0100

    Modified: ultra_son.py (more precision with average of values)

--- src/ultra_son.py
+++ src/ultra_son.py
@@ -9,12 +9,12 @@ def get_distance(trig_pin, echo_pin):
 		GPIO.output(trig_pin, True)
 		time.sleep(0.00001)
 		GPIO.output(trig_pin, False)
-	while GPIO.input(echo_pin) == 0:
-		pulse = time.time()
-	while GPIO.input(echo_pin) == 1:
-		end_pulse = time.time()
-	distance_list.append(round((end_pulse - pulse) * 340 * 100 / 2, 1))
-	i = i + 1
+		while GPIO.input(echo_pin) == 0:
+			pulse = time.time()
+		while GPIO.input(echo_pin) == 1:
+			end_pulse = time.time()
+		distance_list.append(round((end_pulse - pulse) * 340 * 100 / 2, 1))
+		i = i + 1
 	for dist in distance_list:
 		distance_average = distance_average + dist
 	return distance_average / 10
