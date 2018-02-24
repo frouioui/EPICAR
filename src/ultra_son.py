b3a63e42bb6bc5a1b519a59e0c9aadaa3f660a9c
Poinsard Florent <pflorent@localhost.localdomain>
2018-02-24 17:01:52 +0100

    Modified : syntax in the ultra_son.py

--- src/ultra_son.py
+++ src/ultra_son.py
@@ -2,27 +2,22 @@ import RPi.GPIO as GPIO
 import time
 
 def get_distance(trig_pin, echo_pin):
-    distance_list = []
-    distance_average = 0
-    i = 0
-
-    while i < 10:
-        GPIO.output(trig_pin, True)
-        time.sleep(0.00001)
-        GPIO.output(trig_pin, False)
-
-        while GPIO.input(echo_pin) == 0:
-        pulse = time.time()
-
-        while GPIO.input(echo_pin) == 1:
-        end_pulse = time.time()
-
-        distance_list.append(round((end_pulse - pulse) * 340 * 100 / 2, 1))
-        i = i + 1
-    for dist in distance_list:
-        distance_average = distance_average + dist
-
-    return distance_average / 10
+	distance_list = []
+	distance_average = 0
+	i = 0
+	while i < 10:
+		GPIO.output(trig_pin, True)
+		time.sleep(0.00001)
+		GPIO.output(trig_pin, False)
+	while GPIO.input(echo_pin) == 0:
+		pulse = time.time()
+	while GPIO.input(echo_pin) == 1:
+		end_pulse = time.time()
+	distance_list.append(round((end_pulse - pulse) * 340 * 100 / 2, 1))
+	i = i + 1
+	for dist in distance_list:
+		distance_average = distance_average + dist
+	return distance_average / 10
 
 ##### BEGINNING #####
 GPIO.setmode(GPIO.BCM)
