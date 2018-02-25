0b1df0484e8e70d0a639a760c73770de1b51a9ef
hugo <hugo.courthias@epitech.eu>
2018-02-25 03:12:51 +0100

    DSDFGH

--- test_short.py
+++ test_short.py
@@ -1,12 +1,15 @@
 import RPi.GPIO as GPIO
 from time import sleep
- 
+import commands
+
+cmd = raspivid -o - -t 0 -hf -w 800 -h 400 -fps 24 |cvlc -vvv stream:///dev/stdin --sout '#standard{access=http,mux=ts,dst=:8160}' :demux=h264
+u=commands.getoutput(cmd)
 GPIO.setmode(GPIO.BOARD)
- 
+
 Motor1A = 16
 Motor1B = 18
 Motor1E = 22
- 
+
 Motor2A = 23
 Motor2B = 21
 Motor2E = 19
@@ -14,7 +17,7 @@ Motor2E = 19
 GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
- 
+
 GPIO.setup(Motor2A,GPIO.OUT)
 GPIO.setup(Motor2B,GPIO.OUT)
 GPIO.setup(Motor2E,GPIO.OUT)
@@ -23,26 +26,26 @@ print "Going forwards"
 GPIO.output(Motor1A,GPIO.HIGH)
 GPIO.output(Motor1B,GPIO.LOW)
 GPIO.output(Motor1E,GPIO.HIGH)
- 
+
 GPIO.output(Motor2A,GPIO.HIGH)
 GPIO.output(Motor2B,GPIO.LOW)
 GPIO.output(Motor2E,GPIO.HIGH)
- 
+
 sleep(5)
- 
+
 print "Going backwards"
 GPIO.output(Motor1A,GPIO.LOW)
 GPIO.output(Motor1B,GPIO.HIGH)
 GPIO.output(Motor1E,GPIO.HIGH)
- 
+
 GPIO.output(Motor2A,GPIO.HIGH)
 GPIO.output(Motor2B,GPIO.LOW)
 GPIO.output(Motor2E,GPIO.HIGH)
- 
+
 sleep(5)
- 
+
 print "Now stop"
 GPIO.output(Motor1E,GPIO.LOW)
 GPIO.output(Motor2E,GPIO.LOW)
- 
+
 GPIO.cleanup()
