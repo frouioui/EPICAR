a21f488627b0923781ae93f34a179caccc6e66be
frouioui <florent.poinsard@outlook.fr>
2018-02-25 00:41:13 +0000

    added test

--- src/test_motors.py
+++ src/test_motors.py
@@ -1,7 +1,7 @@
 import RPi.GPIO as GPIO
 from time import sleep
 
-GPIO.setmode(GPIO.BOARD)
+GPIO.setmode(GPIO.BCM)
 
 Motor1A = 16
 Motor1B = 18
@@ -11,14 +11,12 @@ GPIO.setup(Motor1A,GPIO.OUT)
 GPIO.setup(Motor1B,GPIO.OUT)
 GPIO.setup(Motor1E,GPIO.OUT)
 
-print "Turning motor on"
-GPIO.output(Motor1A,GPIO.HIGH)
-GPIO.output(Motor1B,GPIO.LOW)
-GPIO.output(Motor1E,GPIO.HIGH)
+GPIO.output(Motor1A,100)
+GPIO.output(Motor1B,0)
+GPIO.output(Motor1E,100)
 
 sleep(2)
 
-print "Stopping motor"
-GPIO.output(Motor1E,GPIO.LOW)
+GPIO.output(Motor1E,0)
 
 GPIO.cleanup()
